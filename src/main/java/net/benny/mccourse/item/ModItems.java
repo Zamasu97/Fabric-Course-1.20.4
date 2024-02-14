994744402243cf81b9a23bb797bbfce774f4a5fd
Beniamin Ksiezarek <bksiezarek@anegis.com>
2024-02-14 15:55:25 +0100

    fiddling around

---
+++ src/main/java/net/benny/mccourse/item/ModItems.java
@@ -0,0 +1,9 @@
+package net.benny.mccourse.item;
+
+import net.benny.mccourse.MCCourseMod;
+
+public class ModItems {
+    public static void registerModItems(){
+        MCCourseMod.LOGGER.info("Registering mod items for " + MCCourseMod.MOD_ID);
+    }
+}
