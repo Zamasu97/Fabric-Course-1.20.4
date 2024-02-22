071858c66ac5f3a64af1f3622274d5e44edbebe6
Beniamin Ksiezarek <bksiezarek@anegis.com>
2024-02-22 15:44:43 +0100

    sound block tweaks and onSteppedOn fiddling

--- src/main/java/net/benny/mccourse/block/custom/SoundBlock.java
+++ src/main/java/net/benny/mccourse/block/custom/SoundBlock.java
@@ -4,20 +4,36 @@ import net.benny.mccourse.MCCourseMod;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.TargetPredicate;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.predicate.entity.EntityPredicates;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.TypeFilter;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.biome.Biome;
+
+import java.util.List;
 
 public class SoundBlock extends Block {
 
-    boolean boopOnce = true;
+    public static final BooleanProperty STEPPED_ONCE = BooleanProperty.of("stepped_on");
+
+
     public SoundBlock(Settings settings) {
         super(settings);
+        setDefaultState(getDefaultState().with(STEPPED_ONCE, false));
     }
 
     @Override
@@ -50,15 +66,55 @@ public class SoundBlock extends Block {
     }
 
     @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(STEPPED_ONCE);
+    }
+
+    @Override
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
-        if(boopOnce){
+        if(!state.get(STEPPED_ONCE)){
             world.playSound(entity, pos, SoundEvents.BLOCK_NOTE_BLOCK_BIT.value(), SoundCategory.BLOCKS, 1f, 1f);
-            boopOnce = false;
+            world.setBlockState(pos,state.with(STEPPED_ONCE,true));
+            world.scheduleBlockTick(pos,this,10);
         }
 
         super.onSteppedOn(world, pos, state, entity);
-        boopOnce = true;
 
     }
 
+    private static boolean almostEqual(double a, double b, double eps){
+        return Math.abs(a-b)<eps;
+    }
+
+    @Override
+    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        if(state.get(STEPPED_ONCE)){
+            List<LivingEntity> entities = world.getEntitiesByClass(LivingEntity.class, new Box(pos.getX()-10,pos.getY()-10,pos.getZ()-10,pos.getX()+10,pos.getY()+10,pos.getZ()+10), EntityPredicates.VALID_ENTITY);
+            LivingEntity entity = world.getClosestEntity(entities,TargetPredicate.DEFAULT, null, pos.getX(), pos.getY(), pos.getZ());
+
+            if (entity == null){
+                entity = world.getClosestEntity(entities,TargetPredicate.createNonAttackable(), null, pos.getX(), pos.getY(), pos.getZ());
+                MCCourseMod.LOGGER.info("2nd check");
+            }
+
+            if (entity == null){
+                return;
+            }
+
+            //mby calculate vector instead of flat coords - could work better
+
+
+            if(!almostEqual(pos.getX(),entity.getX(),1.3) || !almostEqual(pos.getY(),entity.getY() - 1,0.5) || !almostEqual(pos.getZ(),entity.getZ(),1.3)){
+                MCCourseMod.LOGGER.info(pos.getX() + " " + pos.getY() + " " + pos.getZ() + "||" + entity.getX() + " " + entity.getY() + " " + entity.getZ());
+                world.setBlockState(pos,state.with(STEPPED_ONCE,false));
+            }
+            else{
+                world.scheduleBlockTick(pos,this,10);
+            }
+
+        }
+        super.scheduledTick(state, world, pos, random);
+    }
 }
+//getEntitiesByClass(LivingEntity.class,new Box(pos),LivingEntity::isOnGround).isEmpty()
+
